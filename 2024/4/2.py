d11ade7c27c30d5c88481e8b1bb715e53f03cdc7
Paul Keller <contact.keller.paul@gmail.com>
2024-11-22 12:28:53 +0100

    day4

--- 2024/4/2.py
+++ 2024/4/2.py
@@ -2,9 +2,12 @@ import os, sys
 import time
 
 def main():
-    with open(os.path.join(sys.path[0],"e2.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0],"i2.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    lengths = [int(line) for line in lines]
+    min_length = min(lengths)
+    print(sum([length - min_length for length in lengths]))
 
 if __name__ == "__main__":
     before = time.perf_counter()
--- 2024/4/3.py
+++ 2024/4/3.py
@@ -2,9 +2,13 @@ import os, sys
 import time
 
 def main():
-    with open(os.path.join(sys.path[0],"e3.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0],"i3.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
+    lengths = [int(line) for line in lines]
+    lengths.sort()
+    mid = lengths[len(lengths) // 2]
+    print(sum([abs(mid - length) for length in lengths]))
 
 if __name__ == "__main__":
     before = time.perf_counter()
