35b36ad53e03c505386633dd084996b9a91d9521
Paul Keller <contact.keller.paul@gmail.com>
2024-12-01 16:37:05 +0100

    day 11

--- 2024/11/1.py
+++ 2024/11/1.py
@@ -2,10 +2,29 @@ import os, sys
 import time
 
 def main():
-    with open(os.path.join(sys.path[0],"e1.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0],"i1.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
-
+    rules: dict[str, list[str]] = {}
+    for line in lines:
+        key, values = line.split(":")
+        values = values.split(",")
+        rules[key] = values
+    
+    population: dict[str, int] = {}
+    population["A"] = 1
+    
+    for _ in range(4):
+        new_population = {}
+        for key, values in rules.items():
+            for value in values:
+                new_population.setdefault(value, 0)
+                if key in population:
+                    new_population[value] += population[key]
+        population = new_population
+    print(population)
+    print(sum(population[x] for x in population))
+    
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
--- 2024/11/3.py
+++ 2024/11/3.py
@@ -2,10 +2,39 @@ import os, sys
 import time
 
 def main():
-    with open(os.path.join(sys.path[0],"e3.txt"), "r", encoding="utf-8") as f:
+    with open(os.path.join(sys.path[0],"i3.txt"), "r", encoding="utf-8") as f:
         text = f.read().strip()
         lines = text.split("\n")
-
+    rules: dict[str, list[str]] = {}
+    for line in lines:
+        key, values = line.split(":")
+        values = values.split(",")
+        rules[key] = values
+    
+    
+    
+    smallest = float("inf")
+    biggest = float("-inf")
+    
+    for key in rules.keys():
+        
+        population: dict[str, int] = {}
+        population[key] = 1
+        
+        for _ in range(20):
+            new_population = {}
+            for key, values in rules.items():
+                for value in values:
+                    new_population.setdefault(value, 0)
+                    if key in population:
+                        new_population[value] += population[key]
+            population = new_population
+        # print(population)
+        total = sum(population[x] for x in population)
+        smallest = min(smallest, total)
+        biggest = max(biggest, total)
+    print(biggest - smallest)
+    
 if __name__ == "__main__":
     before = time.perf_counter()
     main()
